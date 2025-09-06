ecd69555e3b2dd357ebff19bed4ca64d2a52fda2
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2025-09-06 21:55:42 +0100

    Storybook installed and is stable after npm install

---
+++ app/components/test-storybook/TestStorybook.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="test-storybook" :class="[elementClasses]" :data-testid="props.dataTestid">
+    <LayoutRow tag="div" variant="full-width" :style-class-passthrough="['mbe-20']">
+      <h1>Test Storybook Component</h1>
+      <slot v-if="titleSlot" name="titleSlot">{{ titleSlot }}</slot>
+      <div class="test-storybook__content">
+        <p>
+          <code>&lt;Icon name="mdi:star"&gt;</code>
+          NuxtIcon test
+        </p>
+        <Icon name="mdi:star" style="width: 48px; height: 48px; color: orange" />
+      </div>
+    </LayoutRow>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  dataTestid: {
+    type: String,
+    default: "test-storybook",
+  },
+  styleClassPassthrough: {
+    type: Array as PropType<string[]>,
+    default: () => [],
+  },
+})
+
+const slots = useSlots()
+
+const { elementClasses } = useStyleClassPassthrough(props.styleClassPassthrough)
+</script>
+
+<style lang="css">
+.test-storybook {
+  border: 1px solid #ccc;
+  padding: 16px;
+  margin: 16px;
+  text-align: center;
+
+  .test-storybook__content {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 24px;
+    border: 1px solid #ccc;
+    padding: 16px;
+    margin-top: 12px;
+  }
+}
+</style>

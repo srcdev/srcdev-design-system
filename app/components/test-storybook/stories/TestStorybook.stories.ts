4cefd42063696f4fa8ef059750890bb12b2eb851
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2025-09-07 13:20:06 +0100

    Storybook - Added static folder config and removed some redundant comments

--- app/components/test-storybook/stories/TestStorybook.stories.ts
+++ app/components/test-storybook/stories/TestStorybook.stories.ts
@@ -1,13 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/vue3"
-
 import StoryBookComponent from "../TestStorybook.vue"
-
-// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 
 const meta = {
   title: "Components/TestStorybook/TestStorybook",
   component: StoryBookComponent,
-  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
   argTypes: {
     layoutTag: {
       options: ["div", "header", "footer", "section"],

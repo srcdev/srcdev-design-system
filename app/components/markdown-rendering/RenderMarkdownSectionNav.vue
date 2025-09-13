2aaf41ec92c288f044fbe91ba3e98979e742fc31
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2025-09-13 22:17:12 +0100

    srcdev-nuxt-components update - also refactored RenderMarkdownSectionNav to use ExpandingPanel

--- app/components/markdown-rendering/RenderMarkdownSectionNav.vue
+++ app/components/markdown-rendering/RenderMarkdownSectionNav.vue
@@ -4,25 +4,35 @@
     :class="[elementClasses]"
     :style="{ '--navWrapperHeight': navWrapperHeight }"
   >
-    <details class="details" :open>
-      <summary @click="handleToggle" class="summary" ref="summaryRef">
+    <ExpandingPanel
+      :animation-duration="300"
+      name="terms-nav"
+      icon-size="medium"
+      :force-opened="open"
+      :style-class-passthrough="['details']"
+    >
+      <template #summary>
         {{ t("pages.legal.terms.pageContent.title") }}
-        <Icon name="bi:caret-down-fill" class="summary-icon"></Icon>
-      </summary>
-      <ul class="link-list">
-        <li v-for="(item, index) in i18nContent" :key="item.sectionLink" class="link-item">
-          <a
-            @click="setActiveLinkIndex(index)"
-            :href="`#${item.sectionLink}`"
-            class="link"
-            :class="{ active: activeLinkIndex === index }"
-          >
-            {{ item.sectionTitle }}
-            <Icon name="material-symbols:arrow-right-alt" class="link-icon" />
-          </a>
-        </li>
-      </ul>
-    </details>
+      </template>
+      <template #icon>
+        <Icon name="bi:caret-down-fill" class="icon" />
+      </template>
+      <template #content>
+        <ul class="link-list">
+          <li v-for="(item, index) in i18nContent" :key="item.sectionLink" class="link-item">
+            <a
+              @click="setActiveLinkIndex(index)"
+              :href="`#${item.sectionLink}`"
+              class="link"
+              :class="{ active: activeLinkIndex === index }"
+            >
+              {{ item.sectionTitle }}
+              <Icon name="material-symbols:arrow-right-alt" class="link-icon" />
+            </a>
+          </li>
+        </ul>
+      </template>
+    </ExpandingPanel>
   </div>
 </template>
 
@@ -59,12 +69,6 @@ const open = computed(() => {
   return props.forceExpanded
 })
 
-const handleToggle = (event: Event) => {
-  if (props.forceExpanded) {
-    event.preventDefault()
-  }
-}
-
 const activeLinkIndex = ref<number>(0)
 const setActiveLinkIndex = (index: number) => {
   activeLinkIndex.value = index
@@ -94,83 +98,62 @@ onMounted(() => {
     height: 100%;
   }
 
-  .details {
+  .expanding-panel-details {
     background-color: light-dark(var(--gray-0), var(--gray-11));
     border-radius: 8px;
 
-    &:has([open]) .icon {
-      transform: rotate(180deg);
-    }
-
-    .summary {
+    .expanding-panel-summary {
       color: light-dark(var(--gray-7), var(--gray-3));
       padding: 2rem;
       line-height: 1;
       font-size: var(--step-5);
       font-weight: 600;
-
-      display: flex;
-      justify-content: space-between;
-
-      list-style: none;
-      &::-webkit-details-marker,
-      &::marker {
-        display: none;
-      }
-
-      .summary-icon {
-        display: inline-block;
-        transition: transform 0.3s ease;
-        transform: rotate(0deg);
-        font-size: 14px;
-        font-style: normal;
-        font-weight: 900;
-        line-height: 20px;
-        @media screen and (width >= 768px) {
-          display: none;
-        }
-      }
     }
+  }
+  .expanding-panel-content {
+    .inner {
+      .link-list {
+        margin-block: 0;
+        list-style-type: none;
+        padding: 0;
 
-    .link-list {
-      margin-block: 8px;
-      list-style-type: none;
-      padding: 0;
+        background-color: light-dark(var(--gray-0), var(--gray-11));
 
-      .link-item {
-        .link {
-          --_transition-duration: 250ms;
-          --_transition-easing: ease-in-out;
-          color: light-dark(var(--gray-10), var(--gray-1));
-          font-size: var(--step-4);
-          font-style: normal;
-          font-weight: 500;
-          line-height: 1.3;
-          text-decoration: none;
+        .link-item {
+          .link {
+            --_transition-duration: 250ms;
+            --_transition-easing: ease-in-out;
+            color: light-dark(var(--gray-10), var(--gray-1));
+            font-size: var(--step-4);
+            font-style: normal;
+            font-weight: 500;
+            line-height: 1.3;
+            text-decoration: none;
 
-          display: grid;
-          grid-template-columns: 1fr auto;
-          justify-content: space-between;
-          align-items: center;
-          padding: 16px;
-          transition: background-color var(--_transition-easing) var(--_transition-duration);
-
-          .link-icon {
-            display: inline-block;
-            color: light-dark(var(--gray-12), var(--gray-0));
-
-            margin-inline-end: 8px;
-            opacity: 0;
-            transition: opacity var(--_transition-easing) var(--_transition-duration);
-          }
-
-          &:hover,
-          &:focus-visible,
-          &.active {
-            background-color: light-dark(var(--gray-2), var(--gray-10));
+            display: grid;
+            grid-template-columns: 1fr auto;
+            justify-content: space-between;
+            align-items: center;
+            padding: 16px;
+            transition: background-color var(--_transition-easing) var(--_transition-duration);
 
             .link-icon {
-              opacity: 1;
+              display: inline-block;
+              color: light-dark(var(--gray-12), var(--gray-0));
+
+              margin-inline-end: 8px;
+              opacity: 0;
+              transition: opacity var(--_transition-easing) var(--_transition-duration);
+            }
+
+            &:hover,
+            &:focus-visible,
+            &.active {
+              background-color: light-dark(var(--gray-2), var(--gray-10));
+
+              .link-icon {
+                opacity: 1;
+              }
             }
           }
         }
